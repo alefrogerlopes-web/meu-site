759e74f6f51e50759469e3b84451c78a95a5ef1b
alefrogerlopes-web <alefrogerlopes@gmail.com>
2026-06-16 21:57:10 -0400

    Atualizar o game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ function game() {
 
     ctx.clearRect(0, 0, 800, 500);
 
-    // chão
+    // Chão
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, 800, 500);
 
@@ -36,12 +36,11 @@ function game() {
     }
 
     // Desenha o monstro
-    ctx.fillStyle = "red";
-    ctx.fillRect(mobX, mobY, 40, 40);
+    ctx.font = "40px Arial";
+    ctx.fillText("👹", mobX, mobY + 35);
 
     // Desenha o herói
-    ctx.fillStyle = "blue";
-    ctx.fillRect(x, y, 40, 40);
+    ctx.fillText("🧙", x, y + 35);
 
     requestAnimationFrame(game);
 }
