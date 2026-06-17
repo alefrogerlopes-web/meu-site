0674114ce1c210e6da6562e10b97fcf06f411c7f
alefrogerlopes-web <alefrogerlopes@gmail.com>
2026-06-16 20:28:33 -0400

    Atualizar o game.js

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
-q
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+let x = 100;
+let y = 100;
+
+document.addEventListener("keydown", (e)=>{
+
+    if(e.key=="ArrowUp") y-=10;
+    if(e.key=="ArrowDown") y+=10;
+    if(e.key=="ArrowLeft") x-=10;
+    if(e.key=="ArrowRight") x+=10;
+
+});
+
+function game(){
+
+    ctx.clearRect(0,0,800,500);
+
+    // chão
+    ctx.fillStyle="green";
+    ctx.fillRect(0,0,800,500);
+
+    // herói
+    ctx.fillStyle="blue";
+    ctx.fillRect(x,y,40,40);
+
+    requestAnimationFrame(game);
+}
+
+game();
